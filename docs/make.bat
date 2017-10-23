94eca8a177340a3853e6fc9991929aff73e8d092
kdi <k.v.diemen@naktuinbouw.nl>
2017-10-23 13:10:30 +0200

    documentation

--- docs/make.bat
+++ docs/make.bat
@@ -7,9 +7,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=python -msphinx
 )
-set SOURCEDIR=.
-set BUILDDIR=_build
-set SPHINXPROJ=SNPselection
+set SOURCEDIR=source
+set BUILDDIR=build
+set SPHINXPROJ=Snpselection
 
 if "%1" == "" goto help
 
